2cde272a20f9a92f7d805012a119e71318388aab
Musthaq Ahamad <musthu.gm@gmail.com>
2019-07-11 11:55:29 +0530

    Change boilerplate code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,2 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+// normalize css
+import './src/styles/normalize.css';
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,19 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import styled from 'styled-components';
 
 import Header from "./header"
-import "./layout.css"
+
+
+const LayoutDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  background-color: #1A1A1D;
+  width: 100%;
+  height: 100%;
+`;
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -24,24 +34,11 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
+      <LayoutDiv>
+        <Header/>
+
+        { children }
+      </LayoutDiv>
     )}
   />
 )
