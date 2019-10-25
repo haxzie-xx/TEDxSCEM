ddd3baf1daa52b71c6e82e1a2382d5387f275fb1
Afroz Hussain <36099616+coderhawk999@users.noreply.github.com>
2019-10-25 12:14:52 +0530

    Removed tickets link

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -15,7 +15,6 @@ const LandingPage =()=>
         <p className={style.herotitle}><span className={style.red}>TEDx</span> SCEM 2019</p>
         <p className={style.heroslogan}>"How unpredictable are you"</p>
         <p className={style.slogans}>"<span className={style.spin}></span><span className={style.red}>able"</span> </p>
-      <div className={style.date_holder}> <p className={style.date}>Jul 28th</p> <a className={style.HeroButton} href="https://www.townscript.com/e/tedxscem-211323">Get Tickets</a></div>
     </div>
 </Container>
 </div>
